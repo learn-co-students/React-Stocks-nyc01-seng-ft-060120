8caacb89b2e10c7a6f89086c20e7b30ea636062d
shiro-han <shan99business@gmail.com>
2020-08-10 13:02:28 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,33 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
+  const sortHandler = (e) => {
+    props.sortFn(e.target.value)
+  }
+
+  const checkedBool = (value) => {
+    return props.search.sort === value
+  }
+
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={checkedBool("Alphabetically")} onChange={sortHandler}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={checkedBool("Price")} onChange={sortHandler}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(e) => props.filterFn(e.target.value)}>
+          <option value="None">No Filter</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,21 +4,101 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    portfolio: [],
+    search: {
+      sort: null,
+      filter: null
+    }
+  }
+
+  fetchStocks = () => {
+    fetch('http://localhost:4000/stocks')
+    .then(resp => resp.json())
+    .then(data => this.setState({stocks: data}))
+  }
+
+  portAdd = (stock) => {
+    if (this.state.portfolio.find(portObj => portObj.id === stock.id)){
+      window.alert('Stock already in portfolio')
+    } else {
+      this.setState({portfolio: [...this.state.portfolio, stock]})
+    }
+  }
+
+  portRemove = (stock) => {
+    let array = this.state.portfolio
+    let index = array.indexOf(array.find(portObj => portObj.id === stock.id))
+    array.splice(index, 1)
+    this.setState({portfolio: array})
+  }
+
+  sortFn = (type) => {
+    this.setState({search: {
+      sort: type,
+      filter: this.state.search.filter
+    }})
+  }
+
+  filterFn = (type) => {
+    if (type === "None") {type = null}
+    this.setState({search: {
+      sort: this.state.search.sort,
+      filter: type
+    }})
+  }
+
+  filteredStocks = (input) => {
+    let array = [...input]
+
+    if (this.state.search.sort === "Alphabetically") {
+      array.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        } else if (b.name > a.name) {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+    } else if (this.state.search.sort === "Price") {
+      array.sort((a, b) => {
+        if (a.price > b.price) {
+          return 1;
+        } else if (b.price > a.price) {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+    }
+
+    if (this.state.search.filter) {
+      array = array.filter(stock => stock.type === this.state.search.filter)
+    }
+
+    return array;
+  }
+
+  componentDidMount(){
+    this.fetchStocks();
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar search={this.state.search} sortFn={this.sortFn} filterFn={this.filterFn}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.filteredStocks(this.state.stocks)} portAdd={this.portAdd}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolio={this.filteredStocks(this.state.portfolio)} portRemove={this.portRemove}/>
 
             </div>
           </div>
