e802bc457c783849f0767793aed43b379627e6e0
maxcroy1 <max.croy1@gmail.com>
2020-08-10 15:10:29 -0400

    final solution

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,13 +3,19 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+  stocksToRender() {
+    if (this.props.filter) {
+      return this.props.stocks.filter(stock => stock.type === this.props.filter)
+    } else {
+      return this.props.stocks
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.stocksToRender().map(stock => <Stock stock={stock} key={stock.id} clickHandler={this.props.clickHandler}/>)}
       </div>
     );
   }
