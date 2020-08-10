056e9272f4b7d8f2a824c607140341b9f2953c80
shadman-a <shadman@outlook.com>
2020-08-09 21:52:46 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -8,7 +8,7 @@ class PortfolioContainer extends Component {
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+          this.props.stocks.map(stockObj => <Stock key={stockObj.id} stock={stockObj} stockAction={this.props.removeStock}/>)        
           }
       </div>
     );
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -8,7 +8,7 @@ class StockContainer extends Component {
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.props.stocks.map(stockObj => <Stock key={stockObj.id} stock={stockObj} stockAction={this.props.addPortfolio}/>)
         }
       </div>
     );
