f3890147895c0c0edd9f12b653ff538a6a5c9f66
aaltmark <adamaltmark@gmail.com>
2020-08-11 08:21:39 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
-const SearchBar = () => {
+
+const SearchBar = (props) => {
+  const filterChangeHandler = (e) => {
+    props.filterChangeHandler(e)
+  }
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={null} onChange={props.alphabeticalChangeHandler}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={null} onChange={props.priceChangeHandler}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={filterChangeHandler}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,13 +3,17 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  // portfolioClickHandler = () => {
+  //   this.props.clickHandler(this.props.stocks.id)
+  //   console.log(this.props.stock)
+  // }
+
   render() {
+    let stocks = this.props.stocks.map(stockObj => <Stock key={stockObj.id} stock={stockObj} clickHandler={this.props.clickHandler}/>)
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+          {stocks}
       </div>
     );
   }
